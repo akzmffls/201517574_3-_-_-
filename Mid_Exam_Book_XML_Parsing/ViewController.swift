e393c1d029dc14437aea62cb41a57a75d0415952
Jonghyun Kim <jhkim@dit.ac.kr>
2018-10-23 13:09:52 +0900

    Initial Commit

---
+++ Mid_Exam_Book_XML_Parsing/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mid_Exam_Book_XML_Parsing
+//
+//  Created by amadeus on 23/10/2018.
+//  Copyright © 2018 DIT Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
