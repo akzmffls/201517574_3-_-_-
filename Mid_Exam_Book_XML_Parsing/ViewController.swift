28e4a53f017582ea0073f72f65ada53ec8c909cd
D7703_03 <d7703_03@d7703-03ui-imac.local>
2018-10-25 13:51:12 +0900

    중간고사

--- Mid_Exam_Book_XML_Parsing/ViewController.swift
+++ Mid_Exam_Book_XML_Parsing/ViewController.swift
@@ -5,8 +5,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, XMLParserDelegate, UITableViewDelegate, UITableViewDataSource {
     
+    @IBOutlet weak var tableview: UITableView!
     // [title: author] 전체를 저장하기 위한 딕셔너리 배열
     var books:[[String:String]] = []
     
@@ -21,8 +22,30 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
         // Book.xml 화일을 불러와서 Parsing 하기
+        tableview.dataSource = self
+        tableview.delegate = self
+        
+        if let path = Bundle.main.url(forResource: "Book", withExtension: "xml") {
+            if let myParser = XMLParser(contentsOf: path) {
+                myParser.delegate = self
+                if myParser.parse() {
+                    print("파싱 성공")
+                    
+                    print("elements = \(books)")
+                    
+                } else {
+                    print("파싱 오류2")
+                }
+            } else {
+                print("파싱 오류1")
+            }
+        } else {
+            print("XML 파일 없음")
+        }
         
     }
+    
+    
     
     // UITableView Delegate Method
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -30,6 +53,11 @@ class ViewController: UIViewController {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableview.dequeueReusableCell(withIdentifier: "RE", for: indexPath)
+        let item1 = books[indexPath.row]
+        
+        cell.textLabel?.text = item1["title"]
+        cell.detailTextLabel?.text = item1["author"]
         
         return cell
     }
@@ -37,6 +65,7 @@ class ViewController: UIViewController {
     
     // XMLParser Delegate Method
     func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String] = [:]) {
+        
         
     }
     
